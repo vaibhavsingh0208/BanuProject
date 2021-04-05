df17ad0b52ddf5002740ec92789bfefd80c3c96c
Vaibhav <vaibhsin@adobe.com>
2021-04-05 23:42:15 +0530

    Update LoginSuccess.js

--- LoginSuccess.js
+++ LoginSuccess.js
@@ -8,13 +8,16 @@ export default class LoginSuccess extends Component {
     this.state = {
       h: false,
       data: {},
-      surveyError: ''
+      surveyError: '',
+      questionData: {},
+      surveyQuestionError: ''
     };
   }
 
   clearSurveyData = () => {
     this.setState({
-      data: {}
+      data: {},
+      questionData: {}
     });
   };
 
@@ -38,11 +41,36 @@ export default class LoginSuccess extends Component {
         console.log('error occured', error);
       });
   };
+
+  getSurveyQuestion = () => {
+    axios
+      .get('http://localhost:3200/surveyQuestion', {
+        params: { surveyId: 1 }
+      })
+      .then(response => {
+        if (response.status === 200) {
+          console.log(JSON.stringify(response.data));
+          this.setState({
+            questionData: response.data
+          });
+          console.log('Survey Question Data', response);
+        } else {
+          let surveyQuestionError = 'Error while processing survey question data';
+          this.setState({ surveyQuestionError });
+          console.log('Survey Questions API failed', response);
+        }
+      })
+      .catch(error => {
+        console.log('error occured', error);
+      });
+  };
   render() {
     console.log(this.state.data);
     const data = [this.state.data];
+    const surveyQuestions = this.state.questionData;
     const isDataAvailable = data[0].surveyId;
-    const columns = [
+    const isSurveyQuestionsAvailable = surveyQuestions.length ? surveyQuestions[0].SurveyID : undefined;
+    const columnsSurvey = [
       {
         title: 'Survey Id',
         dataIndex: 'surveyId'
@@ -69,6 +97,33 @@ export default class LoginSuccess extends Component {
       }
     ];
 
+    const columnsSurveyQuestions = [
+      {
+        title: 'Sno',
+        dataIndex: 'SNo'
+      },
+      {
+        title: 'Question ID',
+        dataIndex: 'QuesID'
+      },
+      {
+        title: 'Question Text',
+        dataIndex: 'QText'
+      },
+      {
+        title: ' Response Type',
+        dataIndex: 'RespType'
+      },
+      {
+        title: 'Survey ID',
+        dataIndex: 'SurveyID'
+      },
+      {
+        title: 'Weights',
+        dataIndex: 'Weights'
+      }
+    ];
+
     return (
       <div>
         {!isDataAvailable ? (
@@ -83,15 +138,34 @@ export default class LoginSuccess extends Component {
         ) : null}
         {isDataAvailable ? (
           <div>
-            <Table style={{ width: '1000px', marginTop: '50px' }} dataSource={data} columns={columns} />
-            <Button
-              type='primary'
-              htmlType='submit'
-              onClick={this.clearSurveyData}
-              style={{ display: 'flex', alignItems: 'flex-start', marginTop: '50px' }}
-            >
-              Clear Survey Data
-            </Button>
+            <div>
+              <Table style={{ width: '1000px', marginTop: '50px' }} dataSource={data} columns={columnsSurvey} />
+              <Button
+                type='primary'
+                htmlType='submit'
+                onClick={this.clearSurveyData}
+                style={{ display: 'flex', alignItems: 'flex-start', marginTop: '50px' }}
+              >
+                Clear Survey Data
+              </Button>
+            </div>
+            <div>
+              <Button
+                type='primary'
+                htmlType='submit'
+                onClick={this.getSurveyQuestion}
+                style={{ display: 'flex', alignItems: 'flex-start', marginTop: '50px' }}
+              >
+                Get Survey Question
+              </Button>
+              {isSurveyQuestionsAvailable ? (
+                <Table
+                  style={{ width: '1000px', marginTop: '50px' }}
+                  dataSource={surveyQuestions}
+                  columns={columnsSurveyQuestions}
+                />
+              ) : null}
+            </div>
           </div>
         ) : null}
       </div>
