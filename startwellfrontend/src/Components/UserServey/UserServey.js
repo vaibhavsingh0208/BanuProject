00499cff2f92611b55c66254effefdaf9ad3d727
Vaibhav <vaibhsin@adobe.com>
2021-04-09 23:47:41 +0530

    Update UserServey.js

--- startwellfrontend/src/Components/UserServey/UserServey.js
+++ startwellfrontend/src/Components/UserServey/UserServey.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Button, Table } from 'antd';
 import axios from 'axios';
 import BucketType from '../BucketType/BucketType';
+import { json } from 'body-parser';
 
 export default class UserServey extends Component {
   constructor() {
@@ -10,7 +11,8 @@ export default class UserServey extends Component {
       bucketInfo: [],
       addBucketClicked: false,
       selectedRowKeys: [],
-      loading: false
+      loading: false,
+      surveyQuestionsFetched: false
     };
   }
 
@@ -40,12 +42,12 @@ export default class UserServey extends Component {
       addBucketClicked: false
     });
     axios
-      .get('http://localhost:9000/displayUserbucket')
+      .get('http://localhost:3200/displayUserbucket')
       .then(response => {
         if (response.status === 200) {
           console.log(JSON.stringify(response.data));
           this.setState({
-            bucketInfo: response.data
+            bucketInfo: [response.data]
           });
           console.log('Survey Bucket', response);
         } else {
@@ -59,9 +61,35 @@ export default class UserServey extends Component {
       });
   };
 
+  getSurveyQuestions = record => {
+    axios
+      .get(`http://localhost:3200/surveyQandOpt?surveyId=${record.SNo}`)
+      .then(response => {
+        if (response.status === 200) {
+          console.log(JSON.stringify(response.data));
+          this.setState({
+            questionInfo: response.data,
+            surveyQuestionsFetched: true,
+            addBucketClicked: false,
+            bucketInfo: []
+          });
+          console.log('Question Bucket', response);
+        } else {
+          let surveyError = 'Error while processing survey bucket';
+          this.setState({ surveyError });
+          console.log('Fetch Question API failed', response);
+        }
+      })
+      .catch(error => {
+        console.log('error occured', error);
+      });
+  };
+
   render() {
     const buckeyDataInfo = this.state.bucketInfo;
     const bucketInfohasData = buckeyDataInfo.length;
+    const quetionDataInfo = this.state.questionInfo;
+    const isQuestionDataAvailable = quetionDataInfo?.length;
     const bucketColumnInfo = [
       {
         title: '#',
@@ -74,10 +102,6 @@ export default class UserServey extends Component {
       {
         title: 'Bucket Description',
         dataIndex: 'BucketDesc'
-      },
-      {
-        title: 'Status',
-        dataIndex: 'status'.toString()
       }
     ];
     const addBucketClicked = this.state.addBucketClicked;
@@ -86,6 +110,7 @@ export default class UserServey extends Component {
       selectedRowKeys,
       onChange: this.onSelectChange
     };
+    const hasSelected = selectedRowKeys.length > 0;
 
     return (
       <div style={{ display: 'flex', flexFlow: 'column' }}>
@@ -106,7 +131,7 @@ export default class UserServey extends Component {
             </Button>
           </div>
         </div>
-        <div id='body' style={{ height: '700px' }}>
+        <div id='body' style={{ height: '700px', marginTop: '40px' }}>
           {buckeyDataInfo && bucketInfohasData ? (
             <div
               style={{
@@ -117,14 +142,79 @@ export default class UserServey extends Component {
               }}
             >
               <Table
-                style={{ width: '70%', height: '100%', cursor: 'pointer' }}
+                style={{ width: '70%', height: '100%' }}
                 dataSource={buckeyDataInfo}
                 columns={bucketColumnInfo}
+                onRow={(record, rowIndex) => {
+                  return {
+                    onClick: event => {
+                      this.getSurveyQuestions(record);
+                    }
+                  };
+                }}
               />
             </div>
           ) : addBucketClicked ? (
             <div style={{ width: '75%', marginTop: '150px' }}>
               <BucketType submitSuccess={false} />
+            </div>
+          ) : quetionDataInfo && isQuestionDataAvailable ? (
+            <div style={{ height: '600px', overflow: 'hidden', overflowY: 'auto' }}>
+              {quetionDataInfo.map((question, index) => (
+                <div>
+                  <div key={index} style={{ display: 'flex', flexFlow: 'column' }}>
+                    <div style={{ display: 'flex', flexFlow: 'row', justifyContent: 'flex-start' }}>
+                      <span style={{ color: 'Tomato' }}>Sno: </span> {question.SNo}
+                    </div>
+                    <div style={{ display: 'flex', flexFlow: 'row', justifyContent: 'flex-start' }}>
+                      <span style={{ color: 'Tomato', fontWeight: 'bold', fontSize: '14px' }}>Survey Id: </span>
+                      {question.SurveyID}
+                    </div>
+                    <div style={{ display: 'flex', flexFlow: 'row', justifyContent: 'flex-start' }}>
+                      <span style={{ color: 'Tomato' }}>Question Id: </span>
+                      {question.QuesID}
+                    </div>
+                    <div style={{ display: 'flex', flexFlow: 'row', justifyContent: 'flex-start' }}>
+                      <span style={{ color: 'Tomato' }}>Question: </span> {question.QText}
+                    </div>
+                    <div style={{ display: 'flex', flexFlow: 'row', justifyContent: 'flex-start' }}>
+                      <span style={{ color: 'Tomato' }}>Response Type: </span> {question.RespType}
+                    </div>
+                    <div style={{ display: 'flex', flexFlow: 'row', justifyContent: 'flex-start' }}>
+                      <span style={{ color: 'Tomato' }}>Weight: </span> {question.Weights}
+                    </div>
+                    <div
+                      style={{
+                        display: 'flex',
+                        flexFlow: 'column',
+                        justifyContent: 'flex-start',
+                        alignItems: 'flex-start'
+                      }}
+                    >
+                      <div>
+                        <span
+                          style={{ display: 'flex', flexFlow: 'column', justifyContent: 'flex-start', color: 'Tomato' }}
+                        >
+                          Options:{' '}
+                        </span>
+                      </div>
+                      <div>
+                        {question.options.map((option, index) => (
+                          <div style={{ display: 'flex', flexFlow: 'column', justifyContent: 'flex-start' }}>
+                            <div style={{ display: 'flex', flexFlow: 'row', justifyContent: 'flex-start' }}>
+                              Id: {option.optionId}
+                            </div>
+                            <div style={{ display: 'flex', flexFlow: 'row', justifyContent: 'flex-start' }}>
+                              Text: {option.OptionText}
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                  <div style={{ border: '1px solid' }} />
+                </div>
+              ))}
             </div>
           ) : null}
         </div>
